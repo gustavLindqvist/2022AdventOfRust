86117a0a1c8ce664990c4a1a2577d76270cf73c2
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-01 14:20:35 +0100

    1 line for real now

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,1 @@
-use std::collections::BinaryHeap;
-fn main() {
-    vec![1,3].iter().for_each(|n| println!("{}",include_str!("source.txt").split("\n\n").map(|s|{s.split('\n').map(|i|i.parse::<i32>().unwrap()).sum()}).collect::<BinaryHeap<i32>>().iter().take(*n).sum::<i32>()));
-}
+fn main() {vec![1,3].iter().for_each(|n| println!("{}",include_str!("source.txt").split("\n\n").map(|s|{s.split('\n').map(|i|i.parse::<i32>().unwrap()).sum()}).collect::<std::collections::BinaryHeap<i32>>().iter().take(*n).sum::<i32>()));}
