fc4e86329868887793507523a2faa74b053199a8
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-01 15:13:54 +0100

    removed bracket

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,1 +1,1 @@
-fn main() {vec![1,3].iter().for_each(|n| println!("{}",include_str!("source.txt").split("\n\n").map(|s|{s.split('\n').map(|i|i.parse::<i32>().unwrap()).sum()}).collect::<std::collections::BinaryHeap<i32>>().iter().take(*n).sum::<i32>()));}
+fn main() {vec![1,3].iter().for_each(|n| println!("{}",include_str!("source.txt").split("\n\n").map(|s|s.split('\n').map(|i|i.parse::<i32>().unwrap()).sum()).collect::<std::collections::BinaryHeap<i32>>().iter().take(*n).sum::<i32>()));}
