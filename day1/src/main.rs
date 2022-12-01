e87bac835c77c9457ae8c13797de2062c7634a9a
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-01 14:15:43 +0100

    shorter one liner

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,4 @@
 use std::collections::BinaryHeap;
 fn main() {
-    println!("First star: {}\nSecond star: {}",include_str!("source.txt").split("\n\n").map(|s|{s.split('\n').map(|i|i.parse::<i32>().unwrap()).fold(0,|x,y|x+y)}).collect::<BinaryHeap<i32>>().iter().take(1).sum::<i32>(), include_str!("source.txt").split("\n\n").map(|s|{s.split('\n').map(|i|i.parse::<i32>().unwrap()).fold(0,|x,y|x+y)}).collect::<BinaryHeap<i32>>().iter().take(3).sum::<i32>());
+    vec![1,3].iter().for_each(|n| println!("{}",include_str!("source.txt").split("\n\n").map(|s|{s.split('\n').map(|i|i.parse::<i32>().unwrap()).sum()}).collect::<BinaryHeap<i32>>().iter().take(*n).sum::<i32>()));
 }
