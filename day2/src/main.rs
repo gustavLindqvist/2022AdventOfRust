c1dfc56155a498022ed5bfff2b73b7cace5799a2
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-02 10:31:17 +0100

    day 2 part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,40 @@
-use std::fs;
 fn main() {
-    let contents =
-        fs::read_to_string("source.txt").expect("Should have been able to read the file");
+    let contents = include_str!("source.txt").split("\n");
+
+    let mut sum = 0;
+    for s in contents {
+        match s {
+            "A X" => sum += 4,
+            "B X" => sum += 1,
+            "C X" => sum += 7,
+            "A Y" => sum += 8,
+            "B Y" => sum += 5,
+            "C Y" => sum += 2,
+            "A Z" => sum += 3,
+            "B Z" => sum += 9,
+            "C Z" => sum += 6,
+            _ => sum += 0,
+        }
+    }
+
+    println!("{}", sum);
+
+    let contents = include_str!("source.txt").split("\n");
+
+    sum = 0;
+    for s in contents {
+        match s {
+            "A X" => sum += 3,
+            "B X" => sum += 1,
+            "C X" => sum += 2,
+            "A Y" => sum += 4,
+            "B Y" => sum += 5,
+            "C Y" => sum += 6,
+            "A Z" => sum += 8,
+            "B Z" => sum += 9,
+            "C Z" => sum += 7,
+            _ => sum += 0,
+        }
+    }
+    println!("{}", sum);
 }
