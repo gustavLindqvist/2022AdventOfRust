5aca2406bb10c98d731a265e1a7298c442f896ca
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-04 14:24:11 +0100

    day4 done

---
+++ day4/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    star1();
+    star2();
+}
+
+fn star1() {
+    println!(
+        "{}",
+        include_str!("source.txt")
+            .split("\n")
+            .map(|l| l.split_once(',').unwrap())
+            .map(|(t1, t2)| (
+                t1.split_once('-')
+                    .map(|p| (p.0.parse::<i32>().unwrap(), p.1.parse::<i32>().unwrap()))
+                    .unwrap(),
+                t2.split_once('-')
+                    .map(|p| (p.0.parse::<i32>().unwrap(), p.1.parse::<i32>().unwrap()))
+                    .unwrap()
+            ))
+            .filter(|((t11, t12), (t21, t22))| ((t11 >= t21) & (t12 <= t22))
+                | ((t11 <= t21) & (t12 >= t22)))
+            .collect::<Vec<_>>()
+            .len()
+    );
+}
+
+fn star2() {
+    println!(
+        "{}",
+        include_str!("source.txt")
+            .split("\n")
+            .map(|l| l.split_once(',').unwrap())
+            .map(|(t1, t2)| (
+                t1.split_once('-')
+                    .map(|p| (p.0.parse::<i32>().unwrap(), p.1.parse::<i32>().unwrap()))
+                    .unwrap(),
+                t2.split_once('-')
+                    .map(|p| (p.0.parse::<i32>().unwrap(), p.1.parse::<i32>().unwrap()))
+                    .unwrap()
+            ))
+            .filter(|((t11, t12), (t21, t22))| !(((t11 < t21) & (t12 < t21))
+                | ((t11 > t21) & (t11 > t22))))
+            .collect::<Vec<_>>()
+            .len()
+    );
+}
