66759c62a6d749499b0367dd2c7a34873285a90d
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-06 16:34:50 +0100

    oneline fixed

--- day6/src/oneline.rs
+++ day6/src/oneline.rs
@@ -1,1 +1,1 @@
-pub fn oneline() {[4, 14].iter().for_each(|&l| {println!("{}",[include_str!("source.txt").chars().collect::<Vec<char>>()].iter().map(|s| (l..).map(|i| {s[i - l..i].iter().collect::<std::collections::HashSet<&char>>().len()}).enumerate().filter(|(_, v)| *v == l).nth(0).unwrap().0+ l).collect::<Vec<_>>()[0])});}
+pub fn oneline(){[4,14].iter().for_each(|(&l)|{println!("{}",include_str!("source.txt").chars().collect::<Vec<char>>().windows(l).enumerate().filter(|(_,w)|w.iter().collect::<std::collections::HashSet<_>>().len()==l as usize).nth(0).unwrap().0+l)});}
