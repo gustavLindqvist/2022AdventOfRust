9c0994c7dce03df3590cbd45a2f24efab9c7d016
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-13 11:33:49 +0100

    day13 part 1 done

--- day12/src/oneline.rs
+++ day12/src/oneline.rs
@@ -1,1 +1,54 @@
-pub fn oneline() {}
+use std::collections::HashMap;
+pub fn oneline() {
+    let mut map: Vec<Vec<u8>> = include_str!("data.in")
+        .lines()
+        .map(|l| l.as_bytes().to_vec())
+        .collect();
+
+    let height = map.len();
+    let width = map[0].len();
+    let mut queue = vec![];
+    let mut visited: HashMap<(usize, usize), i32> = HashMap::new();
+    for y in 0..height {
+        for x in 0..width {
+            if map[y][x] == 'E' as u8 {
+                queue.push((y, x));
+                visited.insert((y, x), 0);
+                map[y][x] = 'z' as u8;
+            }
+        }
+    }
+    while queue.len() > 0 {
+        let (y, x) = queue.first().unwrap().clone();
+        queue.remove(0);
+        let dist = visited.get(&(y, x)).unwrap().clone();
+        if map[y][x] == 'a' as u8 {
+            println!("{}", dist);
+            break;
+        }
+        if y > 0 {
+            if !visited.contains_key(&(y - 1, x)) & (map[y][x] - 1 <= map[y - 1][x]) {
+                visited.insert((y - 1, x), dist + 1);
+                queue.push((y - 1, x));
+            }
+        }
+        if x > 0 {
+            if !visited.contains_key(&(y, x - 1)) & (map[y][x] - 1 <= map[y][x - 1]) {
+                visited.insert((y, x - 1), dist + 1);
+                queue.push((y, x - 1));
+            }
+        }
+        if y < height - 1 {
+            if !visited.contains_key(&(y + 1, x)) & (map[y][x] - 1 <= map[y + 1][x]) {
+                visited.insert((y + 1, x), dist + 1);
+                queue.push((y + 1, x));
+            }
+        }
+        if x < width - 1 {
+            if !visited.contains_key(&(y, x + 1)) & (map[y][x] - 1 <= map[y][x + 1]) {
+                visited.insert((y, x + 1), dist + 1);
+                queue.push((y, x + 1));
+            }
+        }
+    }
+}
--- day13/src/star1.rs
+++ day13/src/star1.rs
@@ -1,2 +1,46 @@
 pub fn star1() {
+    let result = include_str!("data.in")
+        .replace("10", "a")
+        .split("\n\n")
+        .map(|s| s.split_once("\n").unwrap())
+        .map(|s| {
+            (
+                s.0.chars().collect::<Vec<char>>(),
+                s.1.chars().collect::<Vec<char>>(),
+            )
+        })
+        .enumerate()
+        .filter(|(_, t)| valid(t.0.clone(), t.1.clone()))
+        .fold(0, |i, t| i + t.0 + 1);
+    println!("{}", result);
+}
+
+fn valid(mut s1: Vec<char>, mut s2: Vec<char>) -> bool {
+    let mut res = true;
+    for i in 0..s1.len() {
+        if !(s1[i] == s2[i]) {
+            match (s1[i], s2[i]) {
+                (_, ']') => {
+                    res = false;
+                    break;
+                }
+                (']', _) => {
+                    break;
+                }
+                (_, '[') => {
+                    s1.insert(i, '[');
+                    s1.insert(i + 2, ']');
+                }
+                ('[', _) => {
+                    s2.insert(i, '[');
+                    s2.insert(i + 2, ']');
+                }
+                (c1, c2) => {
+                    res = c1.to_digit(11).unwrap() < c2.to_digit(11).unwrap();
+                    break;
+                }
+            };
+        }
+    }
+    res
 }
