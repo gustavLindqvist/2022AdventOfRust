5a37c3df7090b798ae9ff8a8af816d2a14e0f080
gustavLindqvist <gustav.lindqvist000@gmail.com>
2022-12-26 13:30:44 +0100

    cleanup

--- day1/target/debug/deps/day1-4398c410b3cf7d0f.d
+++ day1/target/debug/deps/day1-4398c410b3cf7d0f.d
@@ -1,6 +1,5 @@
-/home/gustav/AdventOfRust/day1/target/debug/deps/day1-4398c410b3cf7d0f.rmeta: src/main.rs src/source.txt
+/home/gustav/AdventOfRust/day1/target/debug/deps/day1-4398c410b3cf7d0f.rmeta: src/main.rs
 
-/home/gustav/AdventOfRust/day1/target/debug/deps/day1-4398c410b3cf7d0f.d: src/main.rs src/source.txt
+/home/gustav/AdventOfRust/day1/target/debug/deps/day1-4398c410b3cf7d0f.d: src/main.rs
 
 src/main.rs:
-src/source.txt:
--- day1/target/debug/deps/day1-63ccfe024879e78d.d
+++ day1/target/debug/deps/day1-63ccfe024879e78d.d
@@ -1,6 +1,5 @@
-/home/gustav/AdventOfRust/day1/target/debug/deps/day1-63ccfe024879e78d.rmeta: src/main.rs src/source.txt
+/home/gustav/AdventOfRust/day1/target/debug/deps/day1-63ccfe024879e78d.rmeta: src/main.rs
 
-/home/gustav/AdventOfRust/day1/target/debug/deps/day1-63ccfe024879e78d.d: src/main.rs src/source.txt
+/home/gustav/AdventOfRust/day1/target/debug/deps/day1-63ccfe024879e78d.d: src/main.rs
 
 src/main.rs:
-src/source.txt:
